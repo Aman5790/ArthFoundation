d5fe08c35e936f92f302c902afd4441892c77752
Aman5790 <ammy.5790@gmail.com>
2014-10-04 20:22:41 +0530

    Changes done as per meet on 4-Oct-2014 (friday)

--- js/view/ViewInitialize.js
+++ js/view/ViewInitialize.js
@@ -19,7 +19,7 @@ ViewInitialize.prototype = {
                                           {"Q":"Do you have mild joint pain with a family history of grandparents or parents suffering from Arthritis?","A":"NULL"},\
                                           {"Q":"Does the nagging knee pain disturb your night sleep?","A":"NULL"},\
                                           {"Q":"Do you avoid climbing stairs because of pain in joints?","A":"NULL"},\
-                                          {"Q":"Is the pain & difficulty in activity has been tier for more than 3 months?","A":"NULL"},\
+                                          {"Q":"Is the pain & difficulty in activity has been there for more than 3 months?","A":"NULL"},\
                                           {"Q":"Do you feel continuous or intermittent stiffness & swelling in the joints?","A":"NULL"},\
                                           {"Q":"Is going for family outings and neighborhood place of worship for prayers getting extremely difficult for you?","A":"NULL"},\
                                           {"Q":"Is sitting on floor & squatting extremely painful?","A":"NULL"}\
