7f7978303d4914acf0326f0972f02de434e1d38a
Aman5790 <ammy.5790@gmail.com>
2014-11-17 22:38:35 +0530

    Corrections made - 16'Nov'2014

--- js/view/ViewInitialize.js
+++ js/view/ViewInitialize.js
@@ -14,11 +14,11 @@ ViewInitialize.prototype = {
     initializeApp: function ()
     { 
         localStorage['QA_Arthritis']='{"QA":[\
-                                          {"Q":"Is getting up from a seated position, say while cooking a painful experience?","A":"NULL"},\
+                                          {"Q":"Is getting up from a seated position a painful experience?","A":"NULL"},\
                                           {"Q":"Do you have difficulty in routine household activities?","A":"NULL"},\
                                           {"Q":"Do you have mild joint pain with a family history of grandparents or parents suffering from Arthritis?","A":"NULL"},\
                                           {"Q":"Does the nagging knee pain disturb your night sleep?","A":"NULL"},\
-                                          {"Q":"Do you avoid climbing stairs because of pain in joints?","A":"NULL"},\
+                                          {"Q":"Do you avoid climbing stairs because of pain in Knees / Ankles?","A":"NULL"},\
                                           {"Q":"Is the pain & difficulty in activity been there for more than 3 months?","A":"NULL"},\
                                           {"Q":"Do you feel continuous or intermittent stiffness & swelling in the joints?","A":"NULL"},\
                                           {"Q":"Is going for family outings and neighborhood place of worship for prayers getting extremely difficult for you?","A":"NULL"},\
